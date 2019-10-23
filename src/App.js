25b520834f9ace2480ac25f78fa888666afd2e12
AnnJBarr <annbarr@hotmail.com>
2019-10-23 19:25:55 +0100

    added state to application

--- src/App.js
+++ src/App.js
@@ -5,16 +5,27 @@ import AddItem from "./AddItem";
 import Item from "./Item";
 import "./App.css";
 
-class App extends React.Component {
+class App extends Component {
+  state = {
+    tasks: [
+      {text: "walk dog", completed: true, date: "2019-10-15", id:1},
+      {text: "walk cat", completed: false, date: "2019-10-23", id:2},
+      {text: "wash dishes", completed: false, date: "2019-10-15", id:3},
+      {text: "do shopping", completed: true, date: "2019-10-21", id:4},
+      {text: "hoover", completed: true, date: "2019-10-28", id:5},
+
+    ]
+  }
+
   render() {
     return (
       <div className="container">
         <Header />
         <AddItem />
-        <ItemCount count={6} />
-        <Item text="buy bin liners" completed={true} />
-        <Item text="walk the cat" completed={false} />
-        <Item text="brush the cat" quantity={5} completed={false} />
+        <ItemCount count={this.state.tasks.length} />
+        {this.state.tasks.map(task => {
+          return <Item text={task.text} completed = {task.completed} key={task.id}/>
+        })}
       </div>
     );
   }
